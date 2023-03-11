868d612bdabfb104df65f6c14a66af4610e4378a
theProgrammingBox <justinboxgea@gmail.com>
2023-03-10 19:36:05 -0800

    h

--- Evolutionary Driven RL/Source.cpp
+++ Evolutionary Driven RL/Source.cpp
@@ -16,22 +16,44 @@ int main()
 	curandCreateGenerator(&curandGenerator, CURAND_RNG_PSEUDO_DEFAULT);
 	curandSetPseudoRandomGeneratorSeed(curandGenerator, 1234ULL);
 
-	const uint32_t rows = 3;
-	const uint32_t cols = 4;
+	const uint32_t INPUTS = 3;
+	const uint32_t OUTPUTS = 2;
 
 	float* gpuInputMatrix;
 	float* gpuWeightMatrix;
 	float* gpuOutputMatrix;
 
-	float* cpuInputMatrix = new float[rows * cols];
+	float* cpuInputMatrix = new float[INPUTS];
+	float* cpuWeightMatrix = new float[INPUTS * OUTPUTS];
+	float* cpuOutputMatrix = new float[OUTPUTS];
 
-	cudaMalloc(&gpuInputMatrix, rows * cols * sizeof(float));
-	cudaMalloc(&gpuWeightMatrix, rows * cols * sizeof(float));
-	cudaMalloc(&gpuOutputMatrix, rows * cols * sizeof(float));
+	cudaMalloc(&gpuInputMatrix, INPUTS << 2);
+	cudaMalloc(&gpuWeightMatrix, INPUTS * OUTPUTS << 2);
+	cudaMalloc(&gpuOutputMatrix, OUTPUTS << 2);
 
-	curandGenerateUniformEx(curandGenerator, gpuInputMatrix, rows * cols, -1, 1);
-	cudaMemcpy(cpuInputMatrix, gpuInputMatrix, rows * cols * sizeof(float), cudaMemcpyDeviceToHost);
-	PrintMatrix(cpuInputMatrix, rows, cols, "Input Matrix");
+	curandGenerateUniformEx(curandGenerator, gpuInputMatrix, INPUTS, -1.0f, 1.0f);
+	curandGenerateUniformEx(curandGenerator, gpuWeightMatrix, INPUTS * OUTPUTS, -1.0f, 1.0f);
+
+	float alpha = 1.0f;
+	float beta = 0.0f;
+	cublasGemmStridedBatchedEx(
+		cublasHandle, CUBLAS_OP_N, CUBLAS_OP_N,
+		OUTPUTS, 1, INPUTS,
+		&alpha,
+		gpuWeightMatrix, CUDA_R_32F, OUTPUTS, 0,
+		gpuInputMatrix, CUDA_R_32F, INPUTS, 0,
+		&beta,
+		gpuOutputMatrix, CUDA_R_32F, OUTPUTS, 0,
+		1, CUDA_R_32F, CUBLAS_GEMM_DEFAULT_TENSOR_OP
+	);
+	
+	cudaMemcpy(cpuInputMatrix, gpuInputMatrix, INPUTS << 2, cudaMemcpyDeviceToHost);
+	cudaMemcpy(cpuWeightMatrix, gpuWeightMatrix, INPUTS * OUTPUTS << 2, cudaMemcpyDeviceToHost);
+	cudaMemcpy(cpuOutputMatrix, gpuOutputMatrix, OUTPUTS << 2, cudaMemcpyDeviceToHost);
+	
+	PrintMatrix(cpuInputMatrix, INPUTS, 1, "Input");
+	PrintMatrix(cpuWeightMatrix, INPUTS, OUTPUTS, "Weight");
+	PrintMatrix(cpuOutputMatrix, OUTPUTS, 1, "Output");
 	
 	curandDestroyGenerator(curandGenerator);
 	cublasDestroy(cublasHandle);
