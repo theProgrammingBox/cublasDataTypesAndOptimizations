16f3aa2f4fb004002301beb1c3661a16df012717
theProgrammingBox <justinboxgea@gmail.com>
2023-03-10 20:46:32 -0800

    half rand gen

--- Evolutionary Driven RL/Header.h
+++ Evolutionary Driven RL/Header.h
@@ -29,21 +29,17 @@ void PrintMatrixFloat(float* arr, uint32_t rows, uint32_t cols, const char* labe
 	printf("\n");
 }
 
-__global__ void curandNormalize(float* temp, half* output, uint32_t size, float min, float range)
+__global__ void curandNormalize(half* output, uint32_t size, float min, float range)
 {
 	uint32_t index = blockIdx.x * blockDim.x + threadIdx.x;
 	if (index < size)
-		output[index] = __float2half(min + range * temp[index]);
+	{
+		output[index] = __float2half(*(uint16_t*)(output + index) * range + min);
+	}
 }
 
 void curandGenerateUniformEx(curandGenerator_t generator, half* output, uint32_t size, float min = -1.0f, float max = 1.0f)
 {
-	/*curandGenerateUniform(generator, output, size >> 1);
-	curandNormalize <<<0.0009765625f * size + 1, 1024>>> (output, size, min, max - min);*/
-
-	float* temp;
-	cudaMalloc(&temp, size << 2);
-	curandGenerateUniform(generator, temp, size);
-	curandNormalize <<<0.0009765625f * size + 1, 1024>>> (temp, output, size, min, max - min);
-	cudaFree(temp);
+	curandGenerate(generator, (uint32_t*)output, size << 1);
+	curandNormalize <<<0.0009765625f * size + 1, 1024>>> (output, size, min, (max - min) * 0.0000152590218967f);
 }
